d26bbd05c669cd653cc7e1c67a5318915fd14054
Ronny <ripunjoy.buddha@gmail.com>
2024-02-05 11:58:01 -0500

    transfers updated for win7 patch

--- transfers.sh
+++ transfers.sh
@@ -1,3 +1,2 @@
-echo "https://teams.microsoft.com/l/message/19:35098cb1-5e76-4d10-997b-99e021561edd_7c7db728-c25c-47ec-99c1-791c2ca3c2f0@unq.gbl.spaces/1706644916823?context=%7B%22contextType%22%3A%22chat%22%7D"
-
+echo "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2019/03/windows6.1-kb4490628-x86_3cdb3df55b9cd7ef7fcb24fc4e237ea287ad0992.msu"
 echo "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2019/09/windows6.1-kb4474419-v3-x86_0f687d50402790f340087c576886501b3223bec6.msu"
